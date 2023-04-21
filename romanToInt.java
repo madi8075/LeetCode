5270b866074694e26118b30b989568b646b0c95a
madi8075 <90355714+madi8075@users.noreply.github.com>
2023-04-20 21:59:47 -0400

    Create romanToInt.java
    passed!

---
+++ romanToInt.java
@@ -0,0 +1,68 @@
+// Madison Manuel
+// 4/20/23
+// LeetCode problem "Romans to Int", Difficulty: Easy
+
+import java.util.*;
+import java.io.*;
+
+public class romanToInt {
+    public static int romanToInt(String s) {
+        int sum = 0;
+        char current;
+        int value = 0;
+		ArrayList<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            current = s.charAt(i);
+            switch (current)
+            {
+                case 'I':
+                    nums.add(1);
+					break;
+                case 'V':
+				nums.add(5);
+					break;
+                case 'X':
+					nums.add(10);
+					break;
+                case 'L':
+					nums.add(50);
+					break;
+                case 'C':
+					nums.add(100);
+					break;
+                case 'D':
+					nums.add(500);
+					break;
+                case 'M':
+					nums.add(1000);
+					break;
+            }
+		}
+		for(int j = 0; j < s.length(); j++)
+		{
+			if(j != (s.length() - 1))
+				if(nums.get(j) < nums.get(j + 1))
+				{
+					sum -= nums.get(j);
+					continue;
+				}
+			
+			sum += nums.get(j);
+		}
+        return sum;
+    }
+
+	public static void main(String args[])
+	{
+		int answer = romanToInt("III");
+		System.out.printf("%d\n", answer);
+		answer = romanToInt("LVIII");
+		System.out.printf("%d\n", answer);
+		answer = romanToInt("MCMXCIV");
+		System.out.printf("%d", answer);
+
+		// Passed this LeetCode problem with a 5ms runtime!
+		// Could have done slightly better iterating backwards in the string...
+	}
+}
