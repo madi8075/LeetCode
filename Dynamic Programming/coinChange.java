cd86728dd99a043cde41e33dd6f36e5162647303
Madison <90355714+madi8075@users.noreply.github.com>
2023-04-23 20:34:18 -0400

    Quick sort implementation
    Not accounting for first coins taking too much from the amount. Going to reprogram to be exclusively dp and not quicksort as it is not necessary

---
+++ Dynamic Programming/coinChange.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class coinChange {
+	static int partition(int[] coins, int low, int high) {
+		int pivot = coins[high];
+		int temp = 0;
+		int i = low - 1;
+
+		for (int j = low; j < high; j++) {
+			if (coins[j] < pivot) {
+				i++;
+				temp = coins[i];
+				coins[i] = coins[j];
+				coins[j] = temp;
+			}
+		}
+		i++;
+		temp = coins[i];
+		coins[i] = coins[high];
+		coins[high] = temp;
+		return i;
+	}
+	static void quickSort(int coins[], int low, int high){
+		if (low < high){
+			int partIndex = partition(coins, low, high);
+			quickSort(coins, low, partIndex - 1);
+			quickSort(coins, partIndex + 1, high);
+		}
+	}
+
+	public static int coinChange(int[] coins, int amount)
+	{
+		int numCoins = 0;
+		int thisCoin = 0;
+		quickSort(coins, 0, coins.length - 1);
+		for(int i = coins.length - 1; i >= 0; i--)
+		{
+			thisCoin = amount / coins[i];
+			amount -= thisCoin * coins[i];
+			numCoins += thisCoin;
+			if(amount == 0)
+				return numCoins;
+		}
+		return -1;
+	}
+
+	public static void main(String args[])
+	{
+		int[] coins = {186, 419, 83, 408};
+		System.out.println(coinChange(coins, 11));
+	}
+}
